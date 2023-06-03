9a8d9a3314875ee2b7c33f1f7c62d745592e5488
Mushigarou <procompte97@gmail.com>
2023-06-03 10:36:37 +0100

    Fix: New way of representing data

--- includes/minirt.h
+++ includes/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: absaid <absaid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 07:58:50 by absaid            #+#    #+#             */
-/*   Updated: 2023/06/01 19:30:58 by absaid           ###   ########.fr       */
+/*   Updated: 2023/06/03 10:30:17 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "../libft/libft.h"
 # include "../libgc/gc.h"
-# include "rt_parser.h"
+# include "../includes/rt_parser.h"
 # include <unistd.h>
 # include <math.h>
 # include <stdio.h>
