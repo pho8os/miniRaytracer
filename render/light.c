cb44fd5e138115897f4b7964aa34308bfe15e154
Abderrahim Said <absaid@e3r4p10.1337.ma>
2023-07-14 11:38:45 +0100

    feat : specular light is done

--- render/light.c
+++ render/light.c
@@ -6,7 +6,7 @@
 /*   By: absaid <absaid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 08:39:47 by mfouadi           #+#    #+#             */
-/*   Updated: 2023/07/14 09:58:19 by absaid           ###   ########.fr       */
+/*   Updated: 2023/07/14 11:34:27 by absaid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,32 @@ void    calcul_sphere_light(t_utils *utils, t_sphere *sp, double t)
 	T->norm = normvec(vecsub(T->inter, utils->T.center));
 	T->lvec = normvec(vecsub(utils->l->pos, T->inter));
 	double dot;
-	
+	t_color speclight;
 	dot = dot_prod(T->norm, T->lvec);
-	(dot < 0) && (dot = 0);
-	T->Rnorm  = normvec(vecsub(vecxnum(T->norm, 2 * dot), T->lvec)) ;
-	t_color diffuse = vecxnum(utils->l->color, dot * utils->l->range);
-	double specular = pow(dot_prod(normvec(T->Rnorm), normvec(vecsub(utils->ray.origin, T->inter))), 90);
-	t_color speclight = vecxnum(utils->l->color, specular ); 
-	t_color ambiant = vecxnum(utils->am->color, utils->am->range);
-	t_color lights = coloradd(coloradd(speclight, diffuse), ambiant);
-	T->color = coloradd(speclight,T->color);
-	T->color = colormix(T->color, lights);
+	if(dot > 0)
+	{
+		T->Rnorm  = normvec(vecsub(vecxnum(T->norm, 2 * dot), T->lvec)) ;
+		t_color diffuse = vecxnum(utils->l->color, dot * utils->l->range);
+		double dot2 = dot_prod(normvec(T->Rnorm), normvec(vecsub(utils->ray.origin, T->inter)));
+		if(dot2 <= 0)
+		{
+			speclight = (t_color){0,0,0};
+		}
+		else 
+		{
+			double specular = pow(dot2, 200);
+			speclight = vecxnum(utils->l->color, specular); 
+		}
+		t_color ambiant = vecxnum(utils->am->color, utils->am->range);
+		t_color lights = coloradd(diffuse, ambiant);
+		T->color = colormix(T->color, lights);
+		T->color = coloradd(T->color, speclight);
+	}
+	else
+	{
+		t_color ambiant = vecxnum(utils->am->color, utils->am->range);
+		T->color = colormix(T->color, ambiant);
+	}	
+
     return ;
 }
