1416d5547f9dca85ad33685bfef5a43fcb4c3cf7
Marouane Fouadi <procompte97@gmail.com>
2023-07-16 10:30:31 +0100

    fix: cylinder is render, still has to be centered when 0,0,0 is its coordinates

--- render/intersections.c
+++ render/intersections.c
@@ -6,7 +6,7 @@
 /*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 06:25:47 by mfouadi           #+#    #+#             */
-/*   Updated: 2023/07/15 12:38:47 by mfouadi          ###   ########.fr       */
+/*   Updated: 2023/07/16 10:29:29 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ bool   intersp(t_utils *utils, t_sphere *sp)
 {
 	t_eq2d sol;
 	double t;
-    
+
 	t = 0;
 	sol.a = dot_prod(utils->ray.direction, utils->ray.direction);
 	sol.b = dot_prod(vecsub(utils->ray.origin, sp->center), utils->ray.direction) * 2;
@@ -52,16 +52,11 @@ bool   intersp(t_utils *utils, t_sphere *sp)
 }
 
 /*
-	a   = D|D - (D|V)^2
-	b/2 = D|X - (D|V)*(X|V)
-	c   = X|X - (X|V)^2 - r*r
-
-
-
-Surface normal is:
-
-   N = nrm( P-C-V*m )
-
+∗ x,y,z coordinates of the center of the cylinder: 50.0,0.0,20.6
+∗ 3d normalized vector of axis of cylinder. In range [-1,1] for each x,y,z axis: 0.0,0.0,1.0
+∗ the cylinder diameter: 14.2
+∗ the cylinder height: 21.42
+∗ R,G,B colors in range [0,255]: 10, 0, 255
 */
 bool	ray_cylinder_intersection(t_utils *utils, t_cylinder *cyl)
 {
@@ -69,17 +64,15 @@ bool	ray_cylinder_intersection(t_utils *utils, t_cylinder *cyl)
 	double	t;
 	double	r;
 	double m[2];
-	t = 0;
-	t_vec	x; // O - C : O:Ray Origin C: Center of the shape 
-	x = vecsub(utils->ray.origin, cyl->center);
+	// O - C : O:Ray Origin C: Center of the shape
+	t_vec	x = vecsub(utils->ray.origin, cyl->center); 
 	t_vec	C = vecxnum(cyl->nvec, cyl->height / 2);
 	t_vec	L = vecxnum(C, cyl->height);
 	t_vec	v = normvec(L);
-	// t_vec	v = cyl->center;
-	//	len( P-A ) = r
-	r = lengthvec(vecsub(utils->ray.direction, v));
+	
+	r = cyl->diam / 2;
+	t = 0;
 
-	//	x = O - C
 	// a   = D|D - (D|V)^2
 	sol.a = dot_prod(utils->ray.direction, utils->ray.direction) - \
 		 pow(dot_prod(utils->ray.direction, v), 2);
@@ -96,18 +89,12 @@ bool	ray_cylinder_intersection(t_utils *utils, t_cylinder *cyl)
 		sol.t1 = (-sol.b - sqrt(sol.delta)) / (2 * sol.a);
 		sol.t2 = (-sol.b + sqrt(sol.delta)) / (2 * sol.a);
 		//    m = D|V*t + X|V
-		m[0] = (dot_prod(utils->ray.direction, v) * sol.t1) + \
-			dot_prod(x, v);
-		// if (m > )
-		m[1] = (dot_prod(utils->ray.direction, v) * sol.t2) + \
-			dot_prod(x, v);
-		if ((m[0] > 0 && m[0] < cyl->height) && m[0] <= m[1])
+		m[0] = (dot_prod(utils->ray.direction, v) * sol.t1) + dot_prod(x, v);
+		m[1] = (dot_prod(utils->ray.direction, v) * sol.t2) + dot_prod(x, v);
+		if ((m[0] > 0 && m[0] < cyl->height) )
 			t = sol.t1;
-		else if ((m[1] > 0 && m[1] < cyl->height) && m[1] < m[0])
+		else if ((m[1] > 0 && m[1] < cyl->height) )
 			t = sol.t2;
-		// else
-		// 	t = (m[0] > m[1]) * m[1] + (m[1] > m[0]) * m[0];
-		// t = (sol.t2 > sol.t1) * sol.t1 + (sol.t1 > sol.t2) * sol.t2;
 	}
 	if ((t < utils->T.t || utils->T.t == -1) && t > EPS)
 	{
